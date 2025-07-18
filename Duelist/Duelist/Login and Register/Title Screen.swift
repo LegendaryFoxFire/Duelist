76b993fffa419c7549c7a5a6f0558d22e51c0f02
Sam <sam@Eshans-Mac-mini.local>
2025-07-18 15:26:54 -0500

    Made a primative title screen

---
+++ Duelist/Duelist/Login and Register/Title Screen.swift
@@ -0,0 +1,31 @@
+//
+//  Title Screen.swift
+//  Duelist
+//
+//  Created by Sam on 18/07/25.
+//
+
+import SwiftUI
+
+struct Title_Screen: View {
+    @State private var navigateToLogin = false
+
+    var body: some View {
+        NavigationStack {
+            VStack {
+                Image("swords")
+                D_Label(title: "Duelist")
+                    .font(.system(size: 80))
+                    .bold(true)
+                D_Label(title: "Tap Screen to Continue")
+                    .font(.caption)
+            }
+            .onTapGesture {
+                navigateToLogin = true
+            }
+            .navigationDestination(isPresented: $navigateToLogin){
+                Login()
+            }
+        }
+    }
+}
